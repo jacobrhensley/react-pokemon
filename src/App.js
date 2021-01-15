478e42426107e9629d70b777b8da86eb9b98a9e9
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-15 15:52:48 -0600

    ADD: completed axios call to pokemon api and displayed first set of pokemon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import PokemonList from './pokemonList'
+import axios from 'axios'
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageURL, setCurrentPageURL] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageURL, setNextPageURL] = useState()
+  const [prevPageURL, setPrevPageURL] = useState()
+
+  useEffect(() => {
+    axios.get(currentPageURL).then(res => {
+      setPokemon(res.data.results.map(p => p.name));
+    })
+  }, [])
+
   return (
-    null
+    <PokemonList pokemon={pokemon} />
   );
 }
 
