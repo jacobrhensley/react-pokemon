0e1c42820fca14674d26612f39ea6b2dc617432a
Jacob Hensley <jacobrhensley3@gmail.com>
2021-01-22 17:33:37 -0600

    refactored layout to be card format with bootstrap

--- src/pokemonList.js
+++ src/pokemonList.js
@@ -5,9 +5,12 @@ export default function pokemonList({ pokemon }) {
             <div className="container">
                 <div className="row">
                     {pokemon.results.map(p => (
-                        <div className="col-3" key={p.name}>
-                            {p.name}
-                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.url.slice(34, p.url.lastIndexOf('/'))}.png`} alt={p}></img>
+                        <div className="card col-3" key={p.name}>
+                            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${p.url.slice(34, p.url.lastIndexOf('/'))}.png`} alt={p} className="card-img-top"/>
+                            <div className="card-body">
+                                <h5 className="card-title">{p.name}</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            </div>
                         </div>
                     ))}
                 </div>
